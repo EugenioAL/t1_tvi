406fa2b2ef2cb80a1fc5600a5ae1701e3648c620
eugenioal <eaof@icomp.ufam.edu.br>
2024-01-16 16:44:47 -0400

    questao1 completa

--- trab1.py
+++ trab1.py
@@ -35,17 +35,49 @@ pixels de borda (com a cor original para os pixels de borda e os pixels de inter
 e outra com os pixels de interior (com a cor original para os pixels de interior e pixels de
 borda em branco).'''
 
-def questao1():
+MAX_VAL = 255
 
-    path_image = 'img1.jpg'
-    image = cv2.imread(path_image)
+imOut = "output"
+
+def questao1(path):
+    image = cv2.imread(path)
+    dim = image.shape
 
     if image is None:
         print("Imagem não encontrada!")
     else:
         image_mat = np.array(image)
+    numVal = 0
 
     print(image_mat[0][0])
+    while(numVal == 0):
+        print('Entre com um valor maior que -256 e menor que 256: ')
+        val = input()
+        try:
+            val = int(val)
+        except Exception as e:
+            print('Entrada invalida!')
+        finally:
+            if val >= MAX_VAL*(-1) and val <= MAX_VAL:
+                numVal = 1
+            else:
+                print('Entrada invalida!')
+
+    for i in range(dim[0]):
+        for j in range(dim[1]):
+            for k in range(dim[2]):
+                temp =image_mat[i][j][k]+val
+                if(temp > MAX_VAL):
+                    image_mat[i][j][k] = MAX_VAL
+                elif(temp < 0):
+                    image_mat[i][j][k] = 0
+                else:
+                    image_mat[i][j][k] = temp
+
+    print(image_mat[0][0])
+    cv2.imwrite(imOut+'1.jpg',image_mat)
+
+
 
 def questao2():
     return 0
@@ -67,6 +99,7 @@ def questao7():
 
 def questao9():
     a = 11
+    path_image = 'img1.jpg'
     while(a!='0'):
         print('Entre com o indice da questao:')
         print('\t1 - Questao 01')
@@ -83,28 +116,28 @@ def questao9():
             print("Fim do Programa.")
 
         elif a == '1':
-            print(a)
+            questao1(path_image)
 
         elif a == '2':
-            print(a)
+            questao2(path_image)
             
         elif a == '3':
-            print(a)
+            questao3(path_image)
 
         elif a == '4':
-            print(a)
+            questao4(path_image)
         
         elif a == '5':
-            print(a)
+            questao5(path_image)
 
         elif a == '6':
-            print(a)
+            questao6(path_image)
         
         elif a == '7':
-            print(a)
+            questao7(path_image)
 
         elif a == '8':
-            print(a)
+            questao8(path_image)
 
         else:
             print("Opção invalida!")
